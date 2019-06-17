9a80c1f4c566f8c5e83e5abd97b068fa3ecd10f4
Nassstasia16 <nassstasia16@gmail.com>
2019-06-16 22:38:32 -0700

    started to implement EN/RU and choosing of the theme

--- WGDictionary.py
+++ WGDictionary.py
@@ -1,42 +1,43 @@
+import random
+
 class wgDictionary():
 
     usedLetters = []
     dictionary = []
     tempDictionary = []
     usedWords = []
-    currentWord = " "
-    dicLetters = ""
+    currentWord = ""
+    alphabet = ""
 
     def __init__(self, path, lang):
         self.path = path
         self.lang = lang
-
 
     def makeDictionary(self):
         FwgDictionary = open(self.path, "r", encoding = "utf8")
         self.tempDictionary = list(map(lambda s: s.strip().lower(), FwgDictionary.readlines()))
 
         self.initLetters()
+        FwgDictionary.close()
         return self.dictionary
 
     def initLetters(self):
         if self.lang == "EN":
-            self.dicLetters == "abcdefghijklmnopqrstuvwxyz"
+            self.alphabet = "abcdefghijklmnopqrstuvwxyz"
 
         if self.lang == "RU":
-            self.dicLetters = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+            self.alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 
-        for letter in self.dicLetters:
+        for letter in self.alphabet:
             self.dictionary.append([])
 
         for word in self.tempDictionary:
-            position = self.dicLetters.index(word[0:1])
+            position = self.alphabet.index(word[0:1].lower())
             self.dictionary[position].append(word)
 
         for i in range(len(self.dictionary)):
             if len(self.dictionary[i]) == 0:
-                self.usedLetters.append(self.dicLetters[i:i+1])
-                # self.addUsedLetters(dicLetters[i:i+1])
+                self.usedLetters.append(self.alphabet[i:i+1])
 
     def addUsedLetters(self, letter):
         self.usedLetters.append(letter)
@@ -48,15 +49,87 @@ class wgDictionary():
             print("letter " + letter.title() + " was added to used letters. I don't know any word starting from this letter.")
 
     def checkLetter(self, letter):
-        index = self.dicLetters.find(letter.lower())
+        index = self.alphabet.find(letter.lower())
         if len(self.dictionary[index]) == 0:
             self.addUsedLetters(letter)
 
+class languages():
+    startPhrases = []
+    yourTurnPhrases = []
+    falsePhrases = []
+    firstinput = ""
 
 
-cities = wgDictionary("data/cities.txt", "RU")
-cities.makeDictionary()
-cities.checkLetter("Ъ")
+
+    def __init__(self, lang):
+        self.lang = lang
+        # self.theme = theme
+        if self.lang == "RU":
+            self.firstinput = "Выбери тип игры - города/животные\n"
+        else:
+            self.firstinput = "Select the type of the game - cities/animals\n"
+
+
+    def printDescription(self):
+
+        if self.lang == "RU":
+            print('''Правила простые - каждый игрок должен сказать слово, которое начинается на последнюю букву слова предыдущего игрока. 
+Чтобы прекратить игру введи "я устал, я ухожу".''')
+
+        elif self.lang == "EN":
+            print('''Let's play!
+The rules are simple. You should type a word, which begins with the last letter of the previous word.
+Type "Q" to stop the game.
+            ''')
+
+    def initStartPhrases(self):
+        if self.lang == "RU":
+            self.startPhrases = ["Ты начинаешь", "Начинай", "Настарт-внимание-маррррррш!!", "Давай же, поехали!"]
+
+        elif self.lang == "EN":
+            self.startPhrases = ["Go!", "You start", "Ready - steady - go!"]
+
+        return(self.startPhrases[random.randint(0,len(self.startPhrases)-1)] + "\n")
+
+    def giveYourTurnPhrases(self):
+        if self.lang == "RU":
+            self.yourTurnPhrases = ["тебя ждем...", "ходи!", "твой ход..."]
+
+        elif self.lang == "EN":
+            self.yourTurnPhrases = ["your turn!", "I'll wait...", "you're up!", "your round", "you're go"]
+
+        return(self.startPhrases[random.randint(0,len(self.startPhrases)-1)] + "\n")
+
+continueGame = True
+quantity = 0
+
+lang = input("Выбери язык/Select the language - RU/EN\n")
+if lang[0] in ['R','r','р','Р']:
+    gameLanguage = languages("RU")
+else:
+    gameLanguage = languages("EN")
+
+type = input(gameLanguage.firstinput)
+
+if type[0] in ['г', 'Г']:
+    words = wgDictionary("data/citiesRU.txt", "RU")
+elif type[0] in ['ж','Ж']:
+    words = wgDictionary("data/animalsRU.txt", "RU")
+elif type[0] in ['c','C']:
+    words = wgDictionary("data/citiesEN.txt", "EN")
+elif type[0] in ['a', 'A']:
+    words = wgDictionary("data/animalsEN.txt", "EN")
+
+words.makeDictionary()
+gameLanguage.printDescription()
+
+while continueGame:
+    if quantity == 0:
+        personWord = input(gameLanguage.initStartPhrases())
+    else:
+        personWord = input(gameLanguage.giveYourTurnPhrases())
+
+
 
 
 
